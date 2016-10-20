ab5693c9dfcf2a81031ca490e335364e61ae9700
Daleon <rugfia@yahoo.com>
2016-10-19 21:27:44 -0600

    Rakefile now works...

--- rakefile.rb
+++ rakefile.rb
@@ -3,10 +3,24 @@ require 'rake/testtask'
 require 'pry'
 
 Rake::TestTask.new do |test_file|
-  t.libs << "test"
-  t.test_files = FileList['test/*_test.rb']
-  t.verbose = true
-  ruby FileList['test/*_test.rb']
-end
+  test_file.libs << "test"
+  test_file.test_files = FileList['test/*_test.rb']
+  test_file.verbose = true
+  test_file_array = FileList['test/*_test.rb']
+  puts "
 
+  ------------- Running test from #{test_file_array[1]}... -------------
+  "
+  ruby test_file_array[1]
+  puts "
+
+  ------------- Running test from #{test_file_array[2]}... -------------
+  "
+  ruby test_file_array[2]
+  puts "
+
+  ------------- Running test from #{test_file_array[0]}... This could take a while... -------------
+  "
+  ruby test_file_array[0]
+end
 task default: :test
